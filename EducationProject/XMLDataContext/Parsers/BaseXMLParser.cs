5261d6d688cf5c614cd54220347fe0f86240558a
JustDimus <evtushenkodmitriy02@gmail.com>
2021-01-01 19:12:45 +0200

    Added User parser

---
+++ EducationProject/XMLDataContext/Parsers/BaseXMLParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using XMLDataContext.Interfaces;
+
+namespace XMLDataContext.Parsers
+{
+    public abstract class BaseXMLParser<T> : IXMLParser<T>
+    {
+        public abstract Func<XElement, T> ParseToClass { get; }
+
+        public abstract Func<T, XElement> ParseToXElement { get; }
+
+        public abstract string ElementName { get; }
+
+        protected XElement DefaultXElement => new XElement(ElementName);
+    }
+}
--- EducationProject/XMLDataContext/Parsers/UserXMLParser.cs
+++ EducationProject/XMLDataContext/Parsers/UserXMLParser.cs
@@ -7,18 +7,37 @@ using XMLDataContext.Interfaces;
 
 namespace XMLDataContext.Parsers
 {
-    public class UserXMLParser : IXMLParser<User>
+    public class UserXMLParser : BaseXMLParser<User>
     {
-        public Func<XElement, User> ParseToClass => (element) =>
+        public override Func<XElement, User> ParseToClass => (element) =>
         {
             return new User()
             {
-
+                Id = Int32.Parse(element.Attribute("Id").Value),
+                FirstName = element.Element("FirstName").Value,
+                SecondName = element.Element("SecondName").Value,
+                PhoneNumber = element.Element("PhoneNumber").Value,
+                Country = element.Element("Country").Value,
+                City = element.Element("City").Value,
+                Mail = element.Element("Mail").Value,
+                BirthDate = DateTime.Parse(element.Element("BirthDate").Value)
             };
         };
 
-        public Func<User, XElement> ParseToXElement => throw new NotImplementedException();
+        public override Func<User, XElement> ParseToXElement => (element) =>
+        {
+            XElement result = DefaultXElement;
+            result.Add(new XAttribute("Id", element.Id));
+            result.Add(new XElement("FirstName", element.FirstName));
+            result.Add(new XElement("SecondName", element.SecondName));
+            result.Add(new XElement("PhoneNumber", element.PhoneNumber));
+            result.Add(new XElement("Country", element.Country));
+            result.Add(new XElement("City", element.City));
+            result.Add(new XElement("Mail", element.Mail));
+            result.Add(new XElement("BirthDate", element.BirthDate));
+            return result;
+        };
 
-        public string ElementName => throw new NotImplementedException();
+        public override string ElementName => "User";
     }
 }
